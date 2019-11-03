875ea3505de2cbe8dc98ec8644277294050fe14f
Mohit Gupta <mohit.gpta28@gmail.com>
2019-11-03 17:54:36 -0600

    added register activity

---
+++ app/src/main/java/gupta/mohit/androidchat/RegisterActivity.java
@@ -0,0 +1,14 @@
+package gupta.mohit.androidchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
